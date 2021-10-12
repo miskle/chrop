c90327e54e7bd2de1e00cc0f5c32d7757036bcdb
miskle <75860411+miskle@users.noreply.github.com>
2021-10-12 17:59:26 -0400

    Update chrop.sh

--- chrop.sh
+++ chrop.sh
@@ -1,4 +1,7 @@
-# make sure dev mode is on.
+# make sure dev mode is on, and unzip this in MyFiles.
 # go to crosh,
 # type "shell"
+# type "cd"
+# type "cd MyFiles"
+# type "bash ./chrop/chrop.sh"
 echo "chrop is loading.."
